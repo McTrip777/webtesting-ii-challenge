15eb39ac68818dc903e4871c48daa9c7e0418671
Jacob McFaul <mcjakedude@gmail.com>
2019-04-09 15:33:08 -0700

    Applied some testing

--- testingii/src/App.test.js
+++ testingii/src/App.test.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+// import Dashboard from './dashboard/Dashboard.js';
 import { render, fireEvent } from 'react-testing-library';
+import 'jest-dom/extend-expect';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
@@ -13,3 +15,26 @@ it('renders successfully', () => {
   render(<App />);
 });
 
+it('adds a strike', () => {
+  const { getByText } = render(<App />);
+
+  const button = getByText('Strike');
+
+  fireEvent.click(button);
+  fireEvent.click(button);
+  
+  // getByText(/Strikes: 1/i);
+  getByText(/Strikes: 2/i);
+});
+
+it('adds a ball', () => {
+  const { getByText } = render(<App />);
+
+  const button = getByText('Ball');
+
+  fireEvent.click(button);
+  // fireEvent.click(button);
+  // fireEvent.click(button);
+  
+  getByText('Balls: 2');
+});
